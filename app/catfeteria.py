0af2a4e7fe45b371804b4aae6cd53e8fba08e184
Aresaavedra <169113151+Aresaavedra@users.noreply.github.com>
2024-08-13 02:37:25 +0000

    agregar las rutas

--- app/catfeteria.py
+++ app/catfeteria.py
@@ -3,7 +3,6 @@ from flask import (
     Blueprint, render_template
 )
 
-from app.db import get_db
 
 bp = Blueprint('catfeteria', __name__, url_prefix='/catfeteria')
 
@@ -11,3 +10,21 @@ bp = Blueprint('catfeteria', __name__, url_prefix='/catfeteria')
 def index():
 
     return render_template('catfeteria/index1.html')
+
+@bp.route('/index1', methods=('GET', 'POST'))
+def index1():
+
+    return render_template('catfeteria/exit.html')
+
+@bp.route('/exit', methods=('GET', 'POST'))
+def exit():
+
+    return render_template('catfeteria/Menu2.html')
+
+@bp.route('/Menu2', methods=('GET', 'POST'))
+def Menu2():
+
+    return render_template('catfeteria/Reserva.html')
+
+
+
